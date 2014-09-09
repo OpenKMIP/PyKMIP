4711c3c7912da1b00b207ecdf137b09f2aa3af2d
Kaitlin Farr <kaitlin.farr@jhuapl.edu>
2014-09-09 10:06:16 -0700

    Adding dependencies to setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,10 @@ setuptools.setup(
     license='Apache License, Version 2.0',
     packages=setuptools.find_packages(exclude=["kmip.tests", "kmip.tests.*"]),
     package_data={'kmip': ['logconfig.ini']},
+    install_requires=[
+        "enum34",
+        "sqlalchemy",
+    ],
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
