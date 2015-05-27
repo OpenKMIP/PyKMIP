f66b287ede586448027a9a85f654935d99b8ff09
Hadi Esiely <hadi.esiely-barrera@jhuapl.edu>
2015-05-27 18:57:40 -0400

    Adding tests for testing symmetric keys
    testing out tests for creating and destroying symmetric keys

--- kmip/tests/integration/services/test_kmip_client.py
+++ kmip/tests/integration/services/test_kmip_client.py
@@ -96,22 +96,22 @@ class TestKMIPClientIntegration(TestCase):
         self.client.close()
         self._shutdown_server()
 
-    def test_create(self):
-        result = self._create_symmetric_key()
-
-        self._check_result_status(result.result_status.enum, ResultStatus,
-                                  ResultStatus.SUCCESS)
-        self._check_object_type(result.object_type.enum, ObjectType,
-                                ObjectType.SYMMETRIC_KEY)
-        self._check_uuid(result.uuid.value, str)
-
-        # Check the template attribute type
-        self._check_template_attribute(result.template_attribute,
-                                       TemplateAttribute, 2,
-                                       [[str, 'Cryptographic Length', int,
-                                         256],
-                                        [str, 'Unique Identifier', str,
-                                         None]])
+    # def test_create(self):
+    #     result = self._create_symmetric_key()
+    #
+    #     self._check_result_status(result.result_status.enum, ResultStatus,
+    #                               ResultStatus.SUCCESS)
+    #     self._check_object_type(result.object_type.enum, ObjectType,
+    #                             ObjectType.SYMMETRIC_KEY)
+    #     self._check_uuid(result.uuid.value, str)
+    #
+    #     # Check the template attribute type
+    #     self._check_template_attribute(result.template_attribute,
+    #                                    TemplateAttribute, 2,
+    #                                    [[str, 'Cryptographic Length', int,
+    #                                      256],
+    #                                     [str, 'Unique Identifier', str,
+    #                                      None]])
 
     def test_get(self):
         credential_type = CredentialType.USERNAME_AND_PASSWORD
@@ -399,3 +399,104 @@ class TestKMIPClientIntegration(TestCase):
         message = utils.build_er_error(Attribute, 'value', expected, observed,
                                        'attribute_value')
         self.assertEqual(expected, observed, message)
+
+    def test_symmetric_key_create(self):
+        result = self._create_symmetric_key()
+        self._check_result_status(result.result_status.enum, ResultStatus,
+                                  ResultStatus.SUCCESS)
+        self._check_object_type(result.object_type.enum, ObjectType,
+                                ObjectType.SYMMETRIC_KEY)
+        self._check_uuid(result.uuid.value, str)
+
+        # Check the template attribute type
+        self._check_template_attribute(result.template_attribute,
+                                       TemplateAttribute, 2,
+                                       [[str, 'Cryptographic Length', int,
+                                         256],
+                                        [str, 'Unique Identifier', str,
+                                         None]])
+
+    def test_symmetric_key_register(self):
+
+    def test_symmetric_key_locate(self):
+
+    def test_symmetric_key_get(self):
+
+    def test_symmetric_key_destroy(self):
+        credential_type = CredentialType.USERNAME_AND_PASSWORD
+        credential_value = {'Username': 'Peter', 'Password': 'abc123'}
+        credential = self.cred_factory.create_credential(credential_type,
+                                                         credential_value)
+        result = self._create_symmetric_key()
+        uuid = result.uuid.value
+
+        # Verify the secret was created
+        result = self.client.get(uuid=uuid, credential=credential)
+
+        self._check_result_status(result.result_status.enum, ResultStatus,
+                                  ResultStatus.SUCCESS)
+        self._check_object_type(result.object_type.enum, ObjectType,
+                                ObjectType.SYMMETRIC_KEY)
+        self._check_uuid(result.uuid.value, str)
+
+        secret = result.secret
+
+        expected = SymmetricKey
+        message = utils.build_er_error(result.__class__, 'type', expected,
+                                       secret, 'secret')
+        self.assertIsInstance(secret, expected, message)
+
+        # Destroy the SYMMETRIC_KEY object
+        result = self.client.destroy(uuid, credential)
+        self._check_result_status(result.result_status.enum, ResultStatus,
+                                  ResultStatus.SUCCESS)
+        self._check_uuid(result.uuid.value, str)
+
+        # Verify the secret was destroyed
+        result = self.client.get(uuid=uuid, credential=credential)
+
+        self._check_result_status(result.result_status.enum, ResultStatus,
+                                  ResultStatus.OPERATION_FAILED)
+
+        expected = ResultReason
+        observed = type(result.result_reason.enum)
+        message = utils.build_er_error(result.result_reason.__class__, 'type',
+                                       expected, observed)
+        self.assertEqual(expected, observed, message)
+
+        expected = ResultReason.ITEM_NOT_FOUND
+        observed = result.result_reason.enum
+        message = utils.build_er_error(result.result_reason.__class__,
+                                       'value', expected, observed)
+        self.assertEqual(expected, observed, message)
+
+
+    def test_private_key_create(self):
+        pass
+
+    def test_private_key_register(self):
+        pass
+
+    def test_private_key_locate(self):
+        pass
+
+    def test_private_key_get(self):
+        pass
+
+    def test_private_key_destroy(self):
+        pass
+
+    def test_public_key_create(self):
+        pass
+
+    def test_public_key_register(self):
+        pass
+
+    def test_public_key_locate(self):
+        pass
+
+    def test_public_key_get(self):
+        pass
+
+    def test_public_key_destroy(self):
+        pass
