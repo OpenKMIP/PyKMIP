ba746775d3bea0497e43e9802a80d38741f0f728
Peter Hamilton <peter.allen.hamilton@gmail.com>
2018-02-20 13:03:55 -0500

    Update the Travis CI config to add policies for integration tests
    This change updates the files used by Travis CI to set up the
    PyKMIP server used for integration testing, specifically adding in
    support for custom operation policies.

--- .travis/run.sh
+++ .travis/run.sh
@@ -5,11 +5,13 @@ set -x
 
 if [[ "${RUN_INTEGRATION_TESTS}" == "1" ]]; then
     sudo mkdir -p /etc/pykmip/certs
+    sudo mkdir -p /etc/pykmip/policies
     cd /etc/pykmip/certs
     sudo openssl req -x509 -subj "/CN=test" -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes
     cd -
     sudo cp ./.travis/pykmip.conf /etc/pykmip/pykmip.conf
     sudo cp ./.travis/server.conf /etc/pykmip/server.conf
+    sudo cp ./.travis/policy.json /etc/pykmip/policies/policy.json
     sudo mkdir /var/log/pykmip
     sudo chmod 777 /var/log/pykmip
     python ./bin/run_server.py &
@@ -17,4 +19,3 @@ if [[ "${RUN_INTEGRATION_TESTS}" == "1" ]]; then
 else
     tox
 fi
-
