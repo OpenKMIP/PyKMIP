63843fa698263209b17756023c0307011ba4044a
Enilton Nascimento <eniltonj@dinamonetworks.com>
2018-02-20 13:39:43 -0300

    Fix params in helper batch script

--- tests/h14x509.cmd
+++ tests/h14x509.cmd
@@ -1,1 +1,1 @@
-hsmcon 10.0.62.14 certusr -pri rsa2k_iti.pem -pri_cer rsa2k_cert_iti.pem -hsm_cer hsm_tls_cert_FIPS.PEM
+hsmcon 10.0.62.14 certusr -pri rsa2k.pem -pri_cer rsa2k_cert.pem -hsm_cer hsm_cert.pem
